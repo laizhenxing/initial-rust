907bc8d490f6a214f9bb553732e1246f026ae164
xingxiaoli <845766579@qq.com>
2023-05-30 15:42:48 +0800

    [update]cargo fmt

--- abi/src/pb/initial.rs
+++ abi/src/pb/initial.rs
@@ -13,8 +13,8 @@ pub struct Response {
 /// Generated client implementations.
 pub mod greeter_client {
     #![allow(unused_variables, dead_code, missing_docs, clippy::let_unit_value)]
-    use tonic::codegen::*;
     use tonic::codegen::http::Uri;
+    use tonic::codegen::*;
     #[derive(Debug, Clone)]
     pub struct GreeterClient<T> {
         inner: tonic::client::Grpc<T>,
@@ -58,9 +58,8 @@ pub mod greeter_client {
                     <T as tonic::client::GrpcService<tonic::body::BoxBody>>::ResponseBody,
                 >,
             >,
-            <T as tonic::codegen::Service<
-                http::Request<tonic::body::BoxBody>,
-            >>::Error: Into<StdError> + Send + Sync,
+            <T as tonic::codegen::Service<http::Request<tonic::body::BoxBody>>>::Error:
+                Into<StdError> + Send + Sync,
         {
             GreeterClient::new(InterceptedService::new(inner, interceptor))
         }
@@ -99,19 +98,17 @@ pub mod greeter_client {
             &mut self,
             request: impl tonic::IntoRequest<super::Request>,
         ) -> std::result::Result<tonic::Response<super::Response>, tonic::Status> {
-            self.inner
-                .ready()
-                .await
-                .map_err(|e| {
-                    tonic::Status::new(
-                        tonic::Code::Unknown,
-                        format!("Service was not ready: {}", e.into()),
-                    )
-                })?;
+            self.inner.ready().await.map_err(|e| {
+                tonic::Status::new(
+                    tonic::Code::Unknown,
+                    format!("Service was not ready: {}", e.into()),
+                )
+            })?;
             let codec = tonic::codec::ProstCodec::default();
             let path = http::uri::PathAndQuery::from_static("/initial.Greeter/SayHello");
             let mut req = request.into_request();
-            req.extensions_mut().insert(GrpcMethod::new("initial.Greeter", "SayHello"));
+            req.extensions_mut()
+                .insert(GrpcMethod::new("initial.Greeter", "SayHello"));
             self.inner.unary(req, path, codec).await
         }
     }
@@ -151,10 +148,7 @@ pub mod greeter_server {
                 max_encoding_message_size: None,
             }
         }
-        pub fn with_interceptor<F>(
-            inner: T,
-            interceptor: F,
-        ) -> InterceptedService<Self, F>
+        pub fn with_interceptor<F>(inner: T, interceptor: F) -> InterceptedService<Self, F>
         where
             F: tonic::service::Interceptor,
         {
@@ -210,13 +204,9 @@ pub mod greeter_server {
                 "/initial.Greeter/SayHello" => {
                     #[allow(non_camel_case_types)]
                     struct SayHelloSvc<T: Greeter>(pub Arc<T>);
-                    impl<T: Greeter> tonic::server::UnaryService<super::Request>
-                    for SayHelloSvc<T> {
+                    impl<T: Greeter> tonic::server::UnaryService<super::Request> for SayHelloSvc<T> {
                         type Response = super::Response;
-                        type Future = BoxFuture<
-                            tonic::Response<Self::Response>,
-                            tonic::Status,
-                        >;
+                        type Future = BoxFuture<tonic::Response<Self::Response>, tonic::Status>;
                         fn call(
                             &mut self,
                             request: tonic::Request<super::Request>,
@@ -249,18 +239,14 @@ pub mod greeter_server {
                     };
                     Box::pin(fut)
                 }
-                _ => {
-                    Box::pin(async move {
-                        Ok(
-                            http::Response::builder()
-                                .status(200)
-                                .header("grpc-status", "12")
-                                .header("content-type", "application/grpc")
-                                .body(empty_body())
-                                .unwrap(),
-                        )
-                    })
-                }
+                _ => Box::pin(async move {
+                    Ok(http::Response::builder()
+                        .status(200)
+                        .header("grpc-status", "12")
+                        .header("content-type", "application/grpc")
+                        .body(empty_body())
+                        .unwrap())
+                }),
             }
         }
     }
